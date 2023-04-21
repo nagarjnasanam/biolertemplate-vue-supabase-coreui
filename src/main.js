56b70725de09be64ca42bd55f2c770a62fcc6773
nagarjnasanam <nagarjuna_sanam@softprodigy.com>
2023-04-21 16:02:39 +0530

    coreui added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import CoreuiVue from '@coreui/vue'
+import CIcon from '@coreui/icons-vue'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+const app = createApp(App)
 
-createApp(App).use(router).mount('#app')
+app.use(router)
+app.use(CoreuiVue)
+app.component('CIcon', CIcon)
+
+app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page </h1>
+    <p className="text-danger">
+      <span className="visually-hidden">Danger: </span>
+      This action is not reversible
+    </p>
+    <CFormCheck id="flexCheckDefault" label="Default checkbox"/>
+<CFormCheck id="flexCheckChecked" label="Checked checkbox" checked />
   </div>
 </template>
